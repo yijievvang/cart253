6460d18b695e16ea42790387eb26d252530cd4a4
yijievvang <70977056+yijievvang@users.noreply.github.com>
2020-09-22 13:08:01 -0400

    ready to publish
    organized the structure

--- exercise/exercise1/js/script.js
+++ exercise/exercise1/js/script.js
@@ -10,11 +10,11 @@ let  b = 100
 let  g = 94
 
 
-let circleSize = 100;
+let circleSize = 1;
 let circleX = 1
 let circleY = 300
 let circleSpeed = 2
-
+//
 // property value. note: cannot have no values
 // let ellipse1 = {
 //   x: 0,
@@ -27,75 +27,66 @@ let circleSpeed = 2
 // Description of setup() goes here.function
 
 function setup() {
-  createCanvas(500+200, 500);
+  createCanvas(500, 500);
 }
-
+//
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
-background(41, 0, 60);
+  background(41, 0, 60);
 //
 // shooting star
-fill(255, 255, 255);
-circleX= circleX + 5
-circle(circleX, circleY, 15)
-circleY= circleY -2
+  fill(247, 241, 188);
+  circleX = circleX + 5
+  circleSize += 5
+  circle(circleX, circleY, circleSize)
+  circleY = circleY -2
 //
 // Head
   stroke(74, 74, 68)
   strokeWeight(5)
   fill(255, 244, 99);
   ellipse(250, 250, 270, 300);
-
-
-
-
-// holes
-
-  // Eyeball
-
+//
+// Eyeball
   stroke(0, 0, 0)
   strokeWeight(3)
   fill(255, 255, 255);
-constrainedMouseX = constrain(mouseX, 230, 260)
-constrainedMouseY = constrain(mouseY, 190, 210)
-// ellipse(250, 210, 100, 100);
-circle(constrainedMouseX, constrainedMouseY, circleSize);
-
+  constrainedMouseX = constrain(mouseX, 230, 260)
+  constrainedMouseY = constrain(mouseY, 190, 210)
+  circle(constrainedMouseX, constrainedMouseY, 100);
+//
 // iris
-    fill(60, 194, 244);
-// ellipse(250, 210, 50, 50)
-    constrainedMouseX = constrain(mouseX, 215, 275)
-    constrainedMouseY = constrain(mouseY, 170, 230)
-    circle(constrainedMouseX, constrainedMouseY, 50, 50)
+  fill(60, 194, 244);
+  constrainedMouseX = constrain(mouseX, 215, 275)
+  constrainedMouseY = constrain(mouseY, 170, 230)
+  circle(constrainedMouseX, constrainedMouseY, 50, 50)
 
-  fill(0, 0, 0);
-ellipse(constrainedMouseX, constrainedMouseY, 20, 35)
+    fill(0, 0, 0);
+    ellipse(constrainedMouseX, constrainedMouseY, 20, 35)
 //
 // cheeks
     stroke(255, 0, 0)
     b = map(mouseX, 50, 250, 385, 100, 250)
     fill(255, b, 99);
-    arc(205, 260, 50, 50, 3, 7, PI + QUARTER_PI, CHORD);
-    point(197,245);
-    point(220,250);
-    point(208,255);
+    arc(180, 255, 50, 50, 3, 7, PI + QUARTER_PI, CHORD);
+    point(175,245);
+      point(190,240);
+        point(184,255);
 // mouth
   stroke(0, 0, 0)
   strokeWeight(4)
   noFill();
-  arc(250, 260, 160, 40, 0, PI);
+  arc(250, 260, 140, 40, 0, PI);
 //
 // Teeth
   stroke(0, 0, 0)
   strokeWeight(3)
-    fill(255, 255, 255);
-    rectMode(250, 250)
-    rect(210, 280, 30, 30);
+  fill(255, 255, 255);
+  rectMode(250, 250)
+  rect(210, 280, 30, 30);
     rect(260, 280, 30, 30);
-
 
 }
 function mousePressed() {
