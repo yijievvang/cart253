dd3f43e1096593994de274834d7c811ba67bbd07
yijievvang <70977056+yijievvang@users.noreply.github.com>
2020-10-06 19:11:36 -0400

    just started Exercise 03: Love, actually

---
+++ README - Copy.md
@@ -0,0 +1,6 @@
+# cart253-creative computation 1
+vbskj ndcjh!
+
+ehehehhehehhe :))))
+
+check out my shop:http://www.
---
+++ exercise/exercise 03 Love, actually/js/script.js
@@ -0,0 +1,101 @@
+let circle1 = {
+  x: undefined,
+  y: 250,
+  vx: 0,
+  vy: 0,
+  speed: 3,
+  size: 100,
+};
+let circle2 = {
+  x: undefined,
+  y: 250,
+  vx: 0,
+  vy: 0,
+  speed: 3,
+  size: 100,
+};
+let state = `simulation`; // can be title, simulation,love,sadness
+
+
+
+function setup() {
+  createCanvas(500, 500);
+
+  setupCircle()
+}
+function setupCircle() {
+    //separate circles
+      circle1.x = width / 3;
+      circle2.x = 2 * width / 3;
+      // Start circles moving in a random direction
+      circle1.vx = random(-circle1.speed, circle1.speed);
+      circle1.vy = random(-circle1.speed, circle1.speed);
+      circle2.vx = random(-circle2.speed, circle2.speed);
+      circle2.vy = random(-circle2.speed, circle2.speed);
+}
+
+function draw() {
+  background(0);
+  if (state === `title`) {
+    title();
+  }
+  else if (state === `simulation`){
+      simulation();
+  }
+  else if (state === `love`){
+    love();
+  }
+  else if (state === `sadness`){
+
+  }
+
+}
+function title() {
+  push();
+  textSize(64);
+  fill(255, 0, 0);
+  textAlign(CENTRE, CENTRE);
+  text(`love`, width/2, height);
+  pop();
+}
+function simulation() {
+   move();
+   checkoffscreen();
+   checkoverlap();
+   displaycircles();
+ }
+
+function move() {
+// move the circle
+  circle1.x = circle1.x + circle1.vx;
+  circle1.y = circle1.y + circle1.vy;
+
+  circle2.x = circle2.x + circle2.vx;
+  circle2.y = circle2.y + circle2.vy;
+}
+function checkoffscreen(){
+  // check if the circle has gone offscreen
+  if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y> width || circle2.x < 0 || circle2.x > height || circle2.y < 0 || circle2.y> height){
+    // sad ending
+  }
+}
+function checkoverlap(){
+  // check if the circles overlap
+    let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+    if (d < circle1.size/2 + circle2.size/2){
+      // happy ending
+    }
+  // check if the circle has gone offscreen
+  if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y> width || circle2.x < 0 || circle2.x > height || circle2.y < 0 || circle2.y> height){
+    // sad ending
+  }
+}
+function displaycircles() {
+  // display the circles
+  ellipse(circle1.x, circle1.y, circle1.size);
+  ellipse(circle2.x, circle2.y, circle2.size);
+}
+
+function mousePressed(){
+
+}
