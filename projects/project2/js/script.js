a534c95fd3a8fb868b414b93f858f5c09a6c53c4
yijievvang <70977056+yijievvang@users.noreply.github.com>
2020-11-04 12:20:02 -0500

    Update script.js

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -1,20 +1,11 @@
-/**************************************************
-Template p5 project
-Pippin Barr
 
-Here is a description of this template p5 project.
-**************************************************/
-
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
-
+createCanvas(windowWidth,windowHeight);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
+ background(0)
 }
