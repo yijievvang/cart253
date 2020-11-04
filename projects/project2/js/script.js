6828cc7af7f3a8a4a6076c05a1bbfd6653b8b12d
yijievvang <70977056+yijievvang@users.noreply.github.com>
2020-11-04 17:15:31 -0500

    Update script.js

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -1,11 +1,36 @@
+let bubbles = [];
 
 function setup() {
-createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
- background(0)
-}
+  background(0);
+  for (i = 0; i < bubbles.length; i++) {
+    bubbles[i].move();
+    bubbles[i].show();
+
+  }
+
+  class Bubble {
+    constructor(tempx, tempy) {
+      this.x = tempx;
+      this.y = tempy;
+      this.r = tempr;
+      this.size = random(40, 80);
+      this.color = color(255);
+    }
+    move() {
+      this.x += random(-1, 1);
+      this.y += random(-1, 1);
+    }
+
+    show() {
+      noStroke();
+      fill(this.color);
+      ellipse(this.x, this.y, this.size);
+    }
+  }
