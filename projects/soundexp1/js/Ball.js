319ec883b77a4b6b158d848e7816946ec730de0a
yijievvang <70977056+yijievvang@users.noreply.github.com>
2020-11-11 01:30:13 -0500

    e6:finished

--- projects/soundexp1/js/Ball.js
+++ projects/soundexp1/js/Ball.js
@@ -1,8 +1,8 @@
 class Ball {
-  constructor(x, y) {
+  constructor(x, y, note) {
     this.x = x;
     this.y = y;
-    this.size = 50;//the diameter of the ball
+    this.size = random(20, 50); //the diameter of the ball
     this.fill = {
       r: random(200, 255),
       g: random(200, 255),
@@ -19,26 +19,38 @@ class Ball {
     this.oscillator.amp(0.1);
     this.oscillator.start();
 
+    // synth
+    this.note = note;
+    this.synth = new p5.PolySynth();
   }
   move() {
     this.x += this.vx;
     this.y += this.vy;
+
+    //  update frequency
+    let d = dist(this.x, this.y, width / 2, height / 2);
+    let maxDist = dist(0, 0, width / 2, height / 2);
+    let newFreq = map(d, 0, maxDist, this.nearFreq, this.farFreq);
+    this.oscillator.freq(newFreq);
   }
   bounce() {
-    if (this.x-this.size/2 < 0 || this.x-this.size/2 > width) {
+    if (this.x - this.size / 2 < 0 || this.x + this.size / 2 > width) {
       this.vx = -this.vx;
     }
 
-    if (this.y-this.size/2 < 0 || this.y-this.size/2 > height) {
+    if (this.y - this.size / 2 < 0 || this.y + this.size / 2 > height) {
       this.vy = -this.vy;
     }
-}
-  display(){
+  }
+
+  playNote() {
+    this.synth.play(this.note, 0.2);
+  }
+  display() {
     push();
-    nostroke();
-    fill(this.fill.r,this.fill.g,this.fill.b);
+    noStroke();
+    fill(this.fill.r, this.fill.g, this.fill.b);
     circle(this.x, this.y, this.size);
     pop();
   }
-
 }
--- projects/soundexp1/js/script.js
+++ projects/soundexp1/js/script.js
@@ -1,25 +1,79 @@
 "use strict";
+
+let mic;
+
+let synth;
+
+
+let keyNotes = [`F3`, `G3`, `Ab3`, `Bb3`, `C3`, `Db3`, `Eb3`, `F4`]; //f-minor
+
+let currentkeyNote = 0;
+
 let balls = [];
+
+let notes = [];
 
 function setup() {
   createCanvas(500, 500);
+
+  //set up mic
+  mic = new p5.AudioIn();
+  mic.start();
+  //when key pressed
+  synth = new p5.PolySynth();
+
+  userStartAudio();
 }
 
 function draw() {
   background(0);
-
+  displayText();
   for (let i = 0; i < balls.length; i++) {
     let ball = balls[i];
     ball.move();
     ball.bounce();
     ball.display();
   }
-}
-  function createBall(x, y) {
-    let ball = new Ball(x, y);
-    balls.push(ball);
-  }
 
-  function mousePressed() {
-    createBall(mouseX, mouseY);
+  jumpingCircle();
+}
+
+// circle moves up and down based on volume
+function jumpingCircle() {
+  // Get the overall volume (between 0 and 1.0)
+  let vol = mic.getLevel();
+  fill(127);
+  stroke(0);
+  // Draw an ellipse with height based on volume
+  let h = map(vol, 0, 1, height, 0);
+  ellipse(width / 2, h - 25, 50, 50);
+}
+
+function keyPressed() {
+  //start the chaser music
+  setInterval(playKeyNote, 400);
+}
+
+function playKeyNote() {
+  let keyNote = keyNotes[currentkeyNote];
+  synth.play(keyNote, 1, 0, 0.2);
+  currentkeyNote = currentkeyNote + 1;
+  if (currentkeyNote === keyNotes.length) {
+    currentkeyNote = 0;
   }
+}
+
+function displayText() {
+  fill(255, 0, 0);
+  text("open your mic. tap to play ", width / 2, height / 2);
+}
+
+function createBall(x, y) {
+  let note = random(notes);
+  let ball = new Ball(x, y, note);
+  balls.push(ball);
+}
+
+function mousePressed() {
+  createBall(mouseX, mouseY);
+}
